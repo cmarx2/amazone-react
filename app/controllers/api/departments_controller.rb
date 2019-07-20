c408a9c6febf386aba82565bf933f57fc6bddd9a
Candice Marx <candice.marx@hotmail.com>
2019-07-20 10:17:26 -0600

    created amazone app

---
+++ app/controllers/api/departments_controller.rb
@@ -0,0 +1,24 @@
+class Api::DepartmentsController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+ 
+  def destroy
+  end 
+  
+  private
+  
+end
+end
---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,23 @@
+class Api::ProductsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+ 
+  def destroy
+  end 
+  
+  private
+  
+end
